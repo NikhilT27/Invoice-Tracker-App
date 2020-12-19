ef7f4499ee651c162e3fb1388eb56cefe3ef63c6
NikhilT27 <nikhilthakare14@gmail.com>
2020-12-19 12:38:54 +0530

    add: Invoice CRUD api includes createInvoice,deleteInvoice

---
+++ models/Invoice.js
@@ -0,0 +1,12 @@
+const { model, Schema } = require("mongoose");
+
+const invoiceSchema = new Schema({
+  email: String,
+  user: String,
+  date: String,
+  amount: String,
+  imageFile: String,
+  status: String,
+});
+
+module.exports = model("Invoice", invoiceSchema);
---
+++ routes/invoice.js
@@ -0,0 +1,72 @@
+var express = require("express");
+var router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Invoice = require("../models/Invoice");
+const checkAuth = require("../util/check-auth");
+
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../util/validators");
+const { response } = require("../app");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("invoice respond with a resource");
+});
+
+router.post("/createInvoice", async function (req, res, next) {
+  let { amount, imageFile } = req.body;
+  let headers = req.headers;
+  try {
+    const user = checkAuth(headers);
+
+    if (amount.trim() === "") {
+      throw new Error("Amount value must not empty");
+    }
+
+    if (imageFile.trim() === "") {
+      throw new Error("Image File value must not empty");
+    }
+
+    const newInvoice = new Invoice({
+      email: user.email,
+      user: user.id,
+      date: new Date().toISOString(),
+      amount,
+      imageFile,
+      status: "Pending",
+    });
+
+    const invoice = await newInvoice.save();
+    console.log(invoice);
+    res.send(invoice);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+router.post("/deleteInvoice", async function (req, res, next) {
+  let { invoiceID } = req.body;
+  let headers = req.headers;
+  try {
+    const user = checkAuth(headers);
+    console.log(user);
+    const invoice = await Invoice.findById(invoiceID);
+    if (user && invoice) {
+      if (user.email === invoice.email) {
+        let deleted = await invoice.delete();
+        if (deleted) {
+          res.send("Invoice deleted successfully");
+        }
+      } else {
+        throw new Error("Action not allowed");
+      }
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = router;
