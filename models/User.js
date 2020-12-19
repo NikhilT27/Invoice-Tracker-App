889d21f5569a4d2bb70a77382d59d7329627e563
NikhilT27 <nikhilthakare14@gmail.com>
2020-12-19 11:11:21 +0530

    add: user register validation

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const { model, Schema } = require("mongoose");
 const userSchema = new Schema({
   email: String,
   password: String,
-  data: String,
 });
 
 module.exports = model("User", userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const {
@@ -31,6 +32,7 @@ router.post("/login", async function (req, res, next) {
     const { valid, errors } = validateLoginInput(email, password);
     if (!valid) {
       res.send({ errors });
+      throw new Error("Invalid");
     }
 
     const user = await User.findOne({ email });
@@ -39,6 +41,48 @@ router.post("/login", async function (req, res, next) {
       res.send({ ...user._doc, id: user._id, token });
     } else {
       res.send({ errors: { user: "User does't Exist" } });
+      throw new Error("User doesn't Exist");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/register", async function (req, res, next) {
+  let { email, password, confirmPassword } = req.body;
+
+  try {
+    const { valid, errors } = validateRegisterInput(
+      email,
+      password,
+      confirmPassword
+    );
+
+    if (!valid) {
+      res.send({ errors });
+      throw new Error("Invalid");
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      res.send({ errors: { username: "This username is already taken" } });
+      throw new Error("This username is already taken");
+    }
+
+    password = await bcrypt.hash(password, 12);
+    console.log(password);
+
+    const newUser = new User({
+      email,
+      password,
+    });
+
+    const result = await newUser.save();
+
+    const token = generateToken(result);
+
+    if (result) {
+      res.send({ ...result._doc, id: result._id, token });
     }
   } catch (error) {
     console.log(error);
